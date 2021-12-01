c2b41fb3205f92b12993d677a76ea58888c9b639
matthew woods <matthew.woods0157@gmail.com>
2021-12-01 20:03:47 +0000

    Adding the play and client js files to the snake-client repo

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = function () {
+    const conn = net.createConnection({
+      host: '165.227.47.243',
+      port: 50541,
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    conn.on("connect", () => {
+      // code that does something when the connection is first established
+      console.log('Welcome everyone');
+    });
+  
+    conn.on("data", (data) => {
+      // code that does something when the connection is first established
+      console.log('Server says:', data);
+    });
+  
+    return conn;
+};
+
+module.exports = {connect};
+
