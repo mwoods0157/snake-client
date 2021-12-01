abdc457443d0b7157d522416675ad395b6fbbed4
matthew woods <matthew.woods0157@gmail.com>
2021-12-01 21:00:13 +0000

    Adding more info to play.js

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { uptime } = require("process");
 
 const connect = function () {
     const conn = net.createConnection({
@@ -13,12 +14,23 @@ const connect = function () {
       // code that does something when the connection is first established
       console.log('Welcome everyone');
       conn.write('Name: MEW');
+      conn.write('Move: up');
+      //conn.write('Move: up');
     });
+    setTimeout(() => {
+        conn.write('Move: up')
+    }, 5000);
+    /*
+    conn.on("connect", () => {
+        // code that does something when the connection is first established
+        conn.write("Move: up");
+    }); */
   
     conn.on("data", (data) => {
       // code that does something when the connection is first established
       console.log('Server says:', data);
     });
+
   
     return conn;
 };
