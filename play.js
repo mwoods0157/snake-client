705caac97ebf8a23a7687ac03d6d234eba0c93ab
matthew woods <matthew.woods0157@gmail.com>
2021-12-01 21:14:08 +0000

    Added ctrl + x exit to play.js

--- play.js
+++ play.js
@@ -1,7 +1,22 @@
 //const net = require("net");
 const {connect} = require('./client');
 
-// establishes a connection with the game server
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    //handleUserInput is the callback
+    stdin.on("data", handleUserInput);
+    return stdin;
+};
+
+const handleUserInput = function () {
+    //If the data is Ctrl + c, exit the process
+    if (key === '\u0003') {
+        process.exit();
+      }
+  };
 
 
 console.log("Connecting ...");
